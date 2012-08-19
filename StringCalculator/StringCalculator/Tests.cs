44103092786ac86137a75f183d467eb01544fea6
Carole Reichenbach <carole_reichenbach@hotmail.com>
2012-08-19 14:53:03 +0100

    calculator return 0 for empty string

---
+++ StringCalculator/StringCalculator/Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace StringCalculator
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void returns_0_if_empty_string()
+        {
+            Calculator calc = new Calculator();
+            
+            int result = calc.Add("");
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            return 0;
+        }
+    }
+}
